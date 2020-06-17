1e8a6696a008c88b02d1ede06f36220940e69004
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-06-17 17:37:27 -0400

    Updates

---
+++ precache-manifest.b1e4fb139b3a318f5ec727df3729a153.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26d17431dc47f7c32a7b01a658bc8ba",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "56c144ebf64aae000e8f",
+    "url": "/ReactPortfolio/static/css/main.4e86e26f.chunk.css"
+  },
+  {
+    "revision": "d1cb4e527160d35c63bd",
+    "url": "/ReactPortfolio/static/js/2.e7615388.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.e7615388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56c144ebf64aae000e8f",
+    "url": "/ReactPortfolio/static/js/main.ca56839a.chunk.js"
+  },
+  {
+    "revision": "4ae37d521d2bb28cdbdb",
+    "url": "/ReactPortfolio/static/js/runtime-main.17f5a5c9.js"
+  },
+  {
+    "revision": "8b888a60ac371883a50a5f212c250821",
+    "url": "/ReactPortfolio/static/media/Burger.8b888a60.JPG"
+  },
+  {
+    "revision": "f1eba499b30227386ea35baf05f39ed7",
+    "url": "/ReactPortfolio/static/media/DailyPlanner.f1eba499.JPG"
+  },
+  {
+    "revision": "8fd0207635391c19f572e371ad2bb8a2",
+    "url": "/ReactPortfolio/static/media/Grand-Canyon.8fd02076.JPG"
+  },
+  {
+    "revision": "cca9f9f0c6092fbafa1afe18a414e57b",
+    "url": "/ReactPortfolio/static/media/NerdBook.cca9f9f0.JPG"
+  },
+  {
+    "revision": "9dc22e7382f41940d98363ccffb69eb8",
+    "url": "/ReactPortfolio/static/media/NoteTaker.9dc22e73.JPG"
+  },
+  {
+    "revision": "df58d0327ecc3e0575a18fb6b7fba7e2",
+    "url": "/ReactPortfolio/static/media/Project.df58d032.JPG"
+  },
+  {
+    "revision": "9393dbdf6076abbd5772c5c9ea1737af",
+    "url": "/ReactPortfolio/static/media/Resume.9393dbdf.pdf"
+  },
+  {
+    "revision": "5e37b7d32d09c69fcbe5ffe5ec111726",
+    "url": "/ReactPortfolio/static/media/Spencer.5e37b7d3.jpg"
+  },
+  {
+    "revision": "bf573f2c063e479a2c6bb148714ec200",
+    "url": "/ReactPortfolio/static/media/WeatherDashboard.bf573f2c.PNG"
+  },
+  {
+    "revision": "37ef8ec718526bb0d7cf1f42501e7d30",
+    "url": "/ReactPortfolio/static/media/brick.37ef8ec7.jpg"
+  },
+  {
+    "revision": "5142034acfe6bc80cafb7baaa0d80291",
+    "url": "/ReactPortfolio/static/media/me.5142034a.jpg"
+  }
+]);
